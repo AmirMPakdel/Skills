ff3927fb1f3c544f2ef9326b4a10f166f845d98a
AssassinViper <amd.amp1969@gmail.com>
2017-10-17 12:48:25 +0330

    Initial commit

---
+++ app/src/main/java/com/coins/black/skills/MainActivity.java
@@ -0,0 +1,13 @@
+package com.coins.black.skills;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
